c6b21c6873488c58f86b9fcb692e47a673c24267
doyoonear <doyoonleee@gmail.com>
2022-09-18 18:57:55 +0900

    fix: Fix mobileheight scroll

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,24 +1,17 @@
 import { PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 import { color } from '@/styles/theme';
-import useWindowDimensions from '@/hooks/useWindowDimensions';
+
 interface LayoutProps {
   bgColor?: string;
 }
 
 function Layout({ children, bgColor }: PropsWithChildren<LayoutProps>) {
-  const { height, width, controlHeight } = useWindowDimensions();
-  const barHeight = height - controlHeight;
-
-  return (
-    <StyledLayout bgColor={bgColor} barHeight={barHeight}>
-      {children}
-    </StyledLayout>
-  );
+  return <StyledLayout bgColor={bgColor}>{children}</StyledLayout>;
 }
 
-const StyledLayout = styled.div<{ bgColor: string; barHeight: number }>`
-  height: ${({ barHeight }) => `calc(100vh - ${barHeight + 6}rem)`};
+const StyledLayout = styled.div<{ bgColor: string }>`
+  height: calc(100vh - 6rem);
   overflow: auto;
   border: 1px solid red;
   padding: 3.6rem 2rem;
